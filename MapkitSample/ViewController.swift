2efcc9558a3214cd58f6362bf5bd35903526aefc
Aldair Martinez <aldair.garcia@kavak.com>
2022-10-10 15:17:35 -0500

    :sparkles: (mapView): Added mapView and button

--- MapkitSample/ViewController.swift
+++ MapkitSample/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    private lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        map.overrideUserInterfaceStyle = .dark
+        return map
+    }()
+    
+    lazy var showDirectionsButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Show directions", for: .normal)
+        button.addTarget(self, action: #selector(showDirections), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        setup()
     }
 
+    @objc private func showDirections() {
+        
+    }
 
+}
+
+private extension ViewController {
+    
+    func setup() {
+        mapView.delegate = self
+        addViews()
+        setConstraints()
+    }
+    
+    func addViews() {
+        view.addSubview(mapView)
+        view.addSubview(showDirectionsButton)
+    }
+    
+    func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: showDirectionsButton.topAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            showDirectionsButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            showDirectionsButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            showDirectionsButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+    }
+    
+}
+
+extension ViewController: MKMapViewDelegate {
+    
 }
 
